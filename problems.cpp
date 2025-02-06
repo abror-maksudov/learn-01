fe353d57cfc5c844cb342c2d0449c68e64cfbbda
abror-maksudov <a.maksudov@newuu.uz>
2025-02-06 11:34:21 +0500

    solve 15 problems

--- problems.cpp
+++ problems.cpp
@@ -113,18 +113,137 @@ int solve_problems_2() {
     // std::cout << LINE + '\n';
 
     // Problem 4
-    double costPerItem, totalCost;
-    int quantity;
+    // double costPerItem, totalCost;
+    // int quantity;
+    //
+    // std::cout << "Enter the cost per item: $";
+    // std::cin >> costPerItem;
+    // std::cout << "Enter the quantity: ";
+    // std::cin >> quantity;
+    //
+    // totalCost = costPerItem * quantity;
+    // std::cout << "Total cost is $" << totalCost << std::endl;
+    // std::cout << LINE + '\n';
 
-    std::cout << "Enter the cost per item: $";
-    std::cin >> costPerItem;
-    std::cout << "Enter the quantity: ";
-    std::cin >> quantity;
+    // Problem 5
+    // int age1, age2, age3, age4;
+    // double averageAge;
+    //
+    // std::cout << "Enter the ages of 4 family members: ";
+    // std::cin >> age1 >> age2 >> age3 >> age4;
+    // averageAge = (age1 + age2 + age3 + age4) / 4.0;
+    // std::cout << "Average age is " << averageAge << '\n';
+    // std::cout << LINE + '\n';
 
-    totalCost = costPerItem * quantity;
-    std::cout << "Total cost is $" << totalCost << std::endl;
-    std::cout << LINE + '\n';
+    // Problem 6
+    // double length, width, area, perimeter;
+    //
+    // std::cout << "Enter the length of the rectangle: ";
+    // std::cin >> length;
+    // std::cout << "Enter the width of the rectangle: ";
+    // std::cin >> width;
+    //
+    // area = length * width;
+    // perimeter = 2 * (length + width);
+    //
+    // std::cout << "The area of the rectangle is: " << area << '\n';
+    // std::cout << "The perimeter of the rectangle is: " << perimeter << '\n';
+    // std::cout << LINE + '\n';
 
+    // Problem 7
+    // int dividend, divisor, quotient, remainder;
+    //
+    // std::cout << "Input the dividend: ";
+    // std::cin >> dividend;
+    // std::cout << "Input the divisor: ";
+    // std::cin >> divisor;
+    //
+    // quotient = dividend / divisor;
+    // remainder = dividend % divisor;
+    //
+    // std::cout << "The quotient of the division is: " << quotient << std::endl;
+    // std::cout << "The remainder of the division is: " << remainder << std::endl;
+    // std::cout << LINE + '\n';
+
+    // Problem 8
+    // double usd, rate, uzs;
+    // std::cout << "Amount in USD: ";
+    // std::cin >> usd;
+    // std::cout << "Exchange rate (1 USD to UZS): ";
+    // std::cin >> rate;
+    // uzs = usd * rate;
+    // std::cout << "The equivalent amount in UZS is: " << uzs << '\n';
+    // std::cout << LINE + '\n';
+
+    // Problem 9
+    // double radius, area;
+    //
+    // std::cout << "Enter radius: ";
+    // std::cin >> radius;
+    // area = 3.14 * radius * radius;
+    // std::cout << "Surface area of circle with r=" << radius << " is " << area << '\n';
+    // std::cout << LINE + '\n';
+
+    // Problem 10
+    // double radius, height, volume;
+    //
+    // std::cout << "Enter radius: ";
+    // std::cin >> radius;
+    // std::cout << "Enter height: ";
+    // std::cin >> height;
+    // volume = 3.14 * radius * radius * height;
+    // std::cout << "Volume of cylinder with r=" << radius << " and h=" << height << " is " << volume << '\n';
+    // std::cout << LINE + '\n';
+
+    // Problem 11
+    // std::cout << "Distance is 450 KM and time is 2 hours\n";
+    // double velocity_km = 450.0 / 2.0;
+    // double velocity_miles = velocity_km * 0.621371;
+    // std::cout << "V = " << velocity_km << "km/h and " << velocity_miles << "miles/h" << '\n';
+    // std::cout << LINE + '\n';
+
+    // Problem 12
+    // int total_minutes, hours, minutes;
+    // std::cout << "Total minutes: ";
+    // std::cin >> total_minutes;
+    // hours = total_minutes / 60;
+    // minutes = total_minutes % 60;
+    // std::cout << "Total time: " << hours << " hours " << minutes << " minutes" << '\n;
+    // std::cout << LINE + '\n';
+
+    // Problem 13
+    // const int FIXED_COSTS = 10000;
+    // double selling_price, variable_cost, break_even_point;
+    // std::cout << "Enter selling price per unit: ";
+    // std::cin >> selling_price;
+    // std::cout << "Enter variable cost per unit: ";
+    // std::cin >> variable_cost;
+    // break_even_point = FIXED_COSTS / (selling_price - variable_cost);
+    // std::cout << "Break-even point: " << break_even_point << '\n;
+    // std::cout << LINE + '\n';
+
+    // Problem 14
+    // double basic_salary, net_salary;
+    // const double TAX_RATE = 0.12;
+    // std::cout << "Enter basic salary: ";
+    // std::cin >> basic_salary;
+    // double tax_amount = basic_salary * TAX_RATE;
+    // net_salary = basic_salary - tax_amount;
+    // std::cout << "Total salary: " << net_salary << '\n;
+    // std::cout << LINE + '\n';
+
+    // Ptoblem 15
+    // double originalPrice, discountPercentage, discountAmount, finalPrice;
+    //
+    // std::cout << "Enter the original price: ";
+    // std::cin >> originalPrice;
+    // std::cout << "Enter the discount percentage: ";
+    // std::cin >> discountPercentage;
+    // discountAmount = (discountPercentage / 100) * originalPrice;
+    // finalPrice = originalPrice - discountAmount;
+    // std::cout << "Discount Amount: " << discountAmount << '\n';
+    // std::cout << "Final Price: " << finalPrice << '\n';
+    // std::cout << LINE + '\n';
 
     return 0;
 }
